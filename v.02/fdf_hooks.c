e1a4c8e6257f4209682a966b788cd985bc439efa
pojea-lo <92981551+pojea-lo@users.noreply.github.com>
2022-03-12 09:34:25 +0100

    Add files via upload

---
+++ v.02/fdf_hooks.c
@@ -0,0 +1,69 @@
+#include "fdf.h"
+
+int	close_key(int keycode, t_conv *hook)//t_conv *hook)//cierra con esc la pantalla
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(hook->gr->mlx, hook->gr->mlx_win);
+		mlx_destroy_image(hook->gr->mlx, hook->gr->img);
+		free (hook->gr->mlx);
+		ft_free(hook, hook->num->y);
+//		system ("leaks fdf");
+		exit(1);
+	}
+	return(0);
+}
+
+int zoom(int keycode, t_conv *hook)//cambia el centro del dibujo
+{
+	if (keycode == 69)
+	{
+		if (hook->num->esc < 2)//condiciones para escalas iniciales muy bajas que no recuperan por el incremento
+			hook->num->esc *= 3;
+		else if (hook->num->esc < 3)
+			hook->num->esc *= 1.5;
+		else
+			hook->num->esc *= 1.334;
+		hook->num->hk = 'h';//condicion para que no entre en esc ni abra ventana nueva
+//		hook->num->xx; 
+		mlx_destroy_window(hook->gr->mlx, hook->gr->mlx_win);
+		mlx_destroy_image(hook->gr->mlx, hook->gr->img);
+		ft_calculs (hook->str, hook->num);
+	}
+	if (keycode == 78)
+	{
+		if (hook->num->esc > 2)//condicion para que no baje mucho el numero y pueda recuperar en funcion del incremento
+			hook->num->esc /= 1.33;
+		else if (hook->num->esc > 1)
+			hook->num->esc /= 1.1;
+		hook->num->hk = 'h';//condicion para que no entre en esc ni abra ventana nueva
+		mlx_destroy_window(hook->gr->mlx, hook->gr->mlx_win);
+		mlx_destroy_image(hook->gr->mlx, hook->gr->img);
+		ft_calculs (hook->str, hook->num);
+	}
+	return (0);
+}
+
+int	chg_center(int button, int x, int y, t_conv *hook)
+{
+	if (button == 1)
+	{
+		hook->num->hk = 'c';//condicion para cambiar el origen
+		hook->num->xx = x / hook->num->esc;
+		hook->num->yy = y / hook->num->esc;
+		mlx_destroy_window(hook->gr->mlx, hook->gr->mlx_win);
+		mlx_destroy_image(hook->gr->mlx, hook->gr->img);
+		ft_calculs (hook->str, hook->num);
+	}
+	return (0);
+}
+
+int	close_button(t_conv *hook)//cierra con boton rojo
+{
+	mlx_destroy_window(hook->gr->mlx, hook->gr->mlx_win);
+	mlx_destroy_image(hook->gr->mlx, hook->gr->img);
+	free (hook->gr->mlx);	
+	ft_free(hook, hook->num->y);
+	system ("leaks fdf");
+	exit(1);
+}
---
+++ v.02/fdf_utils_4.c
@@ -0,0 +1,29 @@
+#include "fdf.h"
+
+//función que imprime puntos
+void	ft_print_pixel(t_mlx *gr, t_num *num, t_str *str)
+{
+	t_num	c;
+
+//	ft_print_mat(str->sin, num->x, num->y);
+	c.i = -1;
+	c.xxx = num->xx;
+	c.yyy = num->yy;
+	while (++c.i < num->x)
+	{
+		c.j = -1;
+		c.k = 0;
+		if (c.i != 0)
+		{
+			c.xxx = c.xxx + 0.75;
+			c.yyy = c.yyy + 0.433;
+		}
+		while (++c.j < num->y)
+		{
+			mlx_pixel_put(gr->mlx, gr->mlx_win, (c.xxx - (c.j * 0.75)) \
+			* num->esc, ((c.yyy + (c.k * 0.433)) - str->sin[c.j][c.i]) \
+			* num->esc, 0xFFFFFF);
+			c.k++;
+		}
+	}
+}
